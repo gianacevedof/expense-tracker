5ea8b197a6257d822404fa959aa55657d974df5c
Giancarlo <gianacevedof@gmail.com>
2026-04-24 17:10:31 -0400

    initial commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Giancarlo Acevedo © {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <>
+      <div className="welcome">
+        <h1>Hello, Gian.</h1>
+        <h3>What's the damage today?</h3>
+      </div>
+
+      <button className="btn">New expense</button>
+      <button className="btn">View summary</button>
+    </>
+  );
+}
+
+export default Home;
